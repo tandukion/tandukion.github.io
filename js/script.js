482a921cf67065a66a6ad3c8ded7abfb1d0076bb
dwindra <tandukion@hotmail.com>
2020-07-27 17:04:54 +0900

    Made the navbar always shown

--- js/script.js
+++ js/script.js
@@ -5,23 +5,6 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#menu-toggle").toggleClass("active");
-    $("#sidebar-wrapper").toggleClass("active");
-    // Toggles the sidebar icon
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    $(this).toggleClass("active");
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
-    $("#sidebar-wrapper").removeClass("active");
-    $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-  });
-
   // Smooth scrolling using jQuery Easing
   $('a.js-scroll-trigger[href*="#"]').click(function() {
     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
